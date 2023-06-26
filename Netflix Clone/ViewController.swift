82943dbb04d7abd9dce5f0052ecbfd0ee9e5b5cf
Polina Poznyak <polinappoznyak@gmail.com>
2023-06-26 20:26:54 +0300

    Init commit

---
+++ Netflix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Polina Poznyak on 26.06.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
